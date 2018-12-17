87575c73bbfd284c3752dd98b5da5c01b7073a77
Michel Suzigan <mfsuzigan@gmail.com>
2018-12-17 19:21:06 -0200

    Adicionando validacoes de request

--- endpoint.tf
+++ endpoint.tf
@@ -18,6 +18,7 @@ resource "aws_api_gateway_method" "notification" {
   resource_id = "${aws_api_gateway_resource.notification.id}"
   http_method = "POST"
   authorization = "NONE"
+  request_validator_id = "${aws_api_gateway_request_validator.payment-notification.id}"
 }
 
 resource "aws_api_gateway_method_response" "ok" {
@@ -38,15 +39,14 @@ resource "aws_api_gateway_integration_response" "ok" {
   status_code = "${aws_api_gateway_method_response.ok.status_code}"
 }
 
-
 resource "aws_api_gateway_integration" "notification-topic" {
   http_method = "${aws_api_gateway_method.notification.http_method}"
-  integration_http_method = "POST"
   resource_id = "${aws_api_gateway_resource.notification.id}"
   rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
-  type = "AWS"
-  uri = "arn:aws:apigateway:${var.region}:sns:action/Publish"
   credentials = "${aws_iam_role.trust-role.arn}"
+  uri = "arn:aws:apigateway:${var.region}:sns:action/Publish"
+  integration_http_method = "POST"
+  type = "AWS"
 
   request_parameters = {
     "integration.request.querystring.TopicArn" = "'${aws_sns_topic.payment-notification-topic.arn}'"
@@ -54,3 +54,44 @@ resource "aws_api_gateway_integration" "notification-topic" {
   }
 }
 
+resource "aws_api_gateway_deployment" "payment-notification"{
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
+  stage_name = "v1"
+}
+
+resource "aws_api_gateway_request_validator" "payment-notification" {
+  name = "payment-notification-validator"
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
+  validate_request_body = true
+}
+
+resource "aws_api_gateway_model" "payment-notification" {
+  content_type = "application/json"
+  name = "payment-notification-model"
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
+
+  schema = <<EOF
+  {
+  "type": "object",
+  "required": [
+  "reference",
+  "type",
+  "body"
+  ],
+  "properties": {
+  "reference": {
+  "type": "string",
+  "pattern": "^(.*)$"
+  },
+  "type": {
+  "type": "string",
+  "pattern": "^(.*)$"
+  },
+  "body": {
+  "type": "string",
+  "pattern": "^(.*)$"
+  }
+  }
+  }
+  EOF
+}
