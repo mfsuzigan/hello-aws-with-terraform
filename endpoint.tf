097c718a9f59ef2ab5daa492a0afb82bc868ee1a
tqi_msuzigan <mfsuzigan@gmail.com>
2018-12-17 13:17:34 -0200

    Ajustando response da integracao

--- endpoint.tf
+++ endpoint.tf
@@ -2,20 +2,20 @@
 
 ## Api Gateway ##
 # api gateway rest api
-resource "aws_api_gateway_rest_api" "payment-notification-api" {
+resource "aws_api_gateway_rest_api" "payment-notification" {
   name = "payment-notification"
   description = "Payment notification"
 }
 
-resource "aws_api_gateway_resource" "payment-notification-resource" {
-  rest_api_id = "${aws_api_gateway_rest_api.payment-notification-api.id}"
-  parent_id = "${aws_api_gateway_rest_api.payment-notification-api.root_resource_id}"
+resource "aws_api_gateway_resource" "notification" {
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
+  parent_id = "${aws_api_gateway_rest_api.payment-notification.root_resource_id}"
   path_part = "notifications"
 }
 
-resource "aws_api_gateway_method" "payment-notification-http-method" {
-  rest_api_id = "${aws_api_gateway_rest_api.payment-notification-api.id}"
-  resource_id = "${aws_api_gateway_resource.payment-notification-resource.id}"
+resource "aws_api_gateway_method" "notification" {
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
+  resource_id = "${aws_api_gateway_resource.notification.id}"
   http_method = "POST"
   authorization = "NONE"
 
@@ -25,18 +25,30 @@ resource "aws_api_gateway_method" "payment-notification-http-method" {
   }
 }
 
-resource "aws_api_gateway_method_response" "payment-notification-response" {
-  rest_api_id = "${aws_api_gateway_rest_api.payment-notification-api.id}"
-  http_method = "${aws_api_gateway_method.payment-notification-http-method.http_method}"
-  resource_id = "${aws_api_gateway_resource.payment-notification-resource.id}"
+resource "aws_api_gateway_method_response" "ok" {
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
+  http_method = "${aws_api_gateway_method.notification.http_method}"
+  resource_id = "${aws_api_gateway_resource.notification.id}"
   status_code = "200"
+
+  response_models {
+      "application/json" = "Empty"
+  }
 }
 
-resource "aws_api_gateway_integration" "payment-notification-integration" {
-  http_method = "${aws_api_gateway_method.payment-notification-http-method.http_method}"
+resource "aws_api_gateway_integration_response" "ok" {
+  http_method = "${aws_api_gateway_method.notification.http_method}"
+  resource_id = "${aws_api_gateway_resource.notification.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
+  status_code = "${aws_api_gateway_method_response.ok.status_code}"
+}
+
+
+resource "aws_api_gateway_integration" "notification-topic" {
+  http_method = "${aws_api_gateway_method.notification.http_method}"
   integration_http_method = "POST"
-  resource_id = "${aws_api_gateway_resource.payment-notification-resource.id}"
-  rest_api_id = "${aws_api_gateway_rest_api.payment-notification-api.id}"
+  resource_id = "${aws_api_gateway_resource.notification.id}"
+  rest_api_id = "${aws_api_gateway_rest_api.payment-notification.id}"
   type = "AWS"
   uri = "arn:aws:apigateway:${var.region}:sns:action/Publish"
   credentials = "${aws_iam_role.trust-role.arn}"
